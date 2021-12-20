d76fd843ec4cf86850f0c101372b5ea2f5604452
TMats <takomats@hotmail.com>
2021-12-20 17:13:07 -0500

    added backend file

---
+++ backend/server.js
@@ -0,0 +1,49 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import Cards from './dbCards';
+
+// App Config
+const app = express();
+const port = process.env.PORT || 8001
+const connection_url = 'mongodb+srv://takomats:Pwl2qMgjLYl0LZtb@cluster0.jnfgi.mongodb.net/tinderdb?retryWrites=true&w=majority'
+
+// Middleware
+
+// DB Config
+mongoose.connect(connection_url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+
+// API Endpoint
+app.get('/', (req, res) => {
+    res.status(200).send('Hello Clever Programmers!')
+})
+
+app.post('/tinder/cards', (req, res) => {
+    const dbCard = req.body;
+
+    Cards.create(dbCard, (err, data) => {
+        if (err) {
+            res.status(500).send(err)
+        } else {
+            res.status(201).send(data)
+        }
+    })
+})
+
+app.get('/tinder/cards', (req, res) => {
+    Cards.find((err, data) => {
+        if (err) {
+            res.status(500).send(err)
+        } else {
+            res.status(200).send(data)
+        }
+        return null;
+    })
+})
+
+// Listener
+app.listen(port, () => {
+    console.log(`listening on localhost: ${port}`)
+})
